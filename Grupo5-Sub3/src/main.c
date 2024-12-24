d309e101ff939f691d4aa3833aad38ed90402815
joao-vitor01 <joao.vitormiranda@discente.univasf.edu.br>
2024-12-24 18:12:32 -0300

    Atualização da main.c

--- Grupo5-Sub3/src/main.c
+++ Grupo5-Sub3/src/main.c
@@ -11,13 +11,68 @@ void converter_comprimento() {
 }
 
 // Funções de conversão de massa
-void converter_massa() {
-    float quilogramas, gramas, toneladas;
-    printf("Digite o valor em quilogramas: ");
-    scanf("%f", &quilogramas);
-    gramas = quilogramas * 1000;
-    toneladas = quilogramas / 1000;
-    printf("%.2f quilogramas = %.2f gramas = %.2f toneladas\n", quilogramas, gramas, toneladas);
+void converter_massa(){
+    int opcao=-1;
+    double valor, resultado;
+    
+    while(opcao!=0){
+    printf("------------------------------\n");
+    printf("CONVERSOR DE MASSA \n");
+    printf("------------------------------\n");
+    printf("1. Quilogramas para Gramas.\n");
+    printf("2. Quilogramas para Toneladas.\n");
+    printf("3. Gramas para Quilogramas.\n");
+    printf("4. Gramas para Toneladas.\n");
+    printf("5. Toneladas para Quilogramas.\n");
+    printf("6. Toneladas para Gramas.\n");
+    printf("0. Sair.\n");
+    printf("Escolha a conversão que deseja fazer:");
+    scanf("%d",&opcao);
+    
+    
+    switch(opcao){
+        case 1: printf("Digite o valor que deseja converter (em Quilogramas):");
+            scanf("%lf", &valor);
+            resultado = valor*1000;
+            printf("%.2f Quilogramas são %.2f Gramas.\n",valor, resultado);
+            break;
+        case 2: printf("Digite o valor que deseja converter (em Quilogramas):");
+            scanf("%lf", &valor);
+            resultado = valor/1000;
+            printf("%.2f Quilogramas são %.2f Toneladas.\n",valor, resultado);
+            break;
+        case 3: printf("Digite o valor que deseja converter (em Gramas):");
+            scanf("%lf", &valor);
+            resultado = valor/1000;
+            printf("%.2f Gramas são %.2f Quilogramas.\n", valor, resultado);
+            break;
+        case 4: printf("Digite o valor que deseja converter (em Gramas):");
+            scanf("%lf", &valor);
+            resultado = valor / 1000000;
+            printf("%.2f Gramas são %.2f Toneladas.\n", valor, resultado);
+            break;
+        case 5: printf("Digite o valor que deseja converter (em Toneladas):");
+            scanf("%lf", &valor);
+            resultado = valor*1000;
+            printf("%.2f Toneladas são %.2f Quilogramas.\n", valor , resultado);
+
+            break;
+        case 6: printf("Digite o valor que deseja converter (em Toneladas):");
+            scanf("%lf", &valor);
+            resultado = valor * 1000000;
+            printf("%.2f Toneladas são %.2f Gramas.\n", valor, resultado);
+
+            break;
+        case 0: printf("Saindo do programa!\n");
+            break;
+        default: printf("Opção Inválida!\n");
+            break;
+        }
+        if(opcao!=0){
+            printf("Deseja continuar?\n 1. Sim 0. Não\n");
+            scanf("%d",&opcao);
+        }
+    }
 }
 
 // Funções de conversão de volume
